5b7dc3bacbd235f9d6376ecb6c60e39442e8fe73
Randall Lough <randallmlough@gmail.com>
2020-04-19 14:59:42 -0700

    removed unused helper function

--- src/charts/line.js
+++ src/charts/line.js
@@ -2,39 +2,6 @@ import Chart from 'chart.js';
 import { datasetColorGenerator } from '../util/styles';
 
 /**
- * @typedef {Object} Data The Options to use in the function createElement.
- * @property {number} data the value of the data point
- * @property {string} label the label for the data point
- * @property {string} [backgroundColor] the background color for the data point
- * @property {string} [borderColor]  the border color for the data point
- */
-
-const toLineChartData = (data = [{}], opts = {}) => {
-  const { labels = [] } = opts;
-  const chartData = {
-    labels: [],
-    datasets: [],
-  };
-
-  const dataset = {
-    label: label,
-    data: [],
-    backgroundColor: '',
-    hoverBackgroundColor: '',
-    borderColor: '',
-    hoverBorderColor: '',
-    borderWidth: 1,
-    hoverBorderWidth: 1,
-  };
-
-  data.forEach((set) => {
-    chartData.datasets.push(set);
-  });
-
-  return chartData;
-};
-
-/**
  *
  *
  * @param {*} ctx
